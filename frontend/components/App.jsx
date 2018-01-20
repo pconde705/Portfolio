676d8b8677516708feb699c4ee6cda76a12cf4ef
Patrick Conde <patrick.conde.costas@gmail.com>
2018-01-19 19:23:54 -0800

    React setup

---
+++ frontend/components/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import Navbar from './navbar';
+import {Route} from 'react-router-dom';
+
+const App = () => (
+    <div>
+        <Navbar />
+    </div>
+)
+
+export default App;
---
+++ frontend/components/navbar.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class Navbar extends React.Component {
+    render() {
+        return (
+            <div>
+                <h1>React Component</h1>
+            </div>
+        )
+    }
+}
+
+export default Navbar;
