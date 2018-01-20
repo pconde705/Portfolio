4bd9709400db8ed56ae303d12bda18cac179c91a
Patrick Conde <patrick.conde.costas@gmail.com>
2018-01-19 22:59:28 -0800

    Started styling navbar

--- frontend/components/navbar.jsx
+++ frontend/components/navbar.jsx
@@ -1,10 +1,15 @@
 import React from 'react';
+import {Link} from 'react-router-dom'
 
 class Navbar extends React.Component {
     render() {
         return (
-            <div>
-                <h1>React Component</h1>
+            <div className="navbar-div">
+                <p>Example</p>
+                <p>Example</p>
+                <p>Center</p>
+                <p>Example</p>
+                <p>Example</p>
             </div>
         )
     }
