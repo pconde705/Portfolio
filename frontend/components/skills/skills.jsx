93e3e0c3b940d57daabd8d0ca35da50d449a2e4e
Patrick Conde <patrick.conde.costas@gmail.com>
2018-01-22 19:48:50 -0800

    animation effect

--- frontend/components/skills/skills.jsx
+++ frontend/components/skills/skills.jsx
@@ -5,7 +5,7 @@ class Skills extends React.Component {
         return (
             <div className="skills-container">
                 <p className="skill-titles">FrontEnd</p>
-                <div className="skills-frontend">
+                <div className="skills-frontend group-divs">
                     <div>
                         <img src="https://s3.amazonaws.com/media-p.slid.es/uploads/alexanderfarennikov/images/1198519/reactjs.png" alt="" />
                         <p>React</p>
@@ -30,9 +30,14 @@ class Skills extends React.Component {
                         <img src="https://avatars3.githubusercontent.com/u/1562726?s=400&v=4" alt="" />
                         <p>D3</p>
                     </div>
+                    <div>
+                        <img src="https://js.foundation/wp-content/uploads/sites/33/2017/02/jquery.png" alt="" />
+                        <p>jQuery</p>
+                    </div>
                 </div>
+                <div className="span-color">
                 <p className="skill-titles">Backend</p>
-                <div className="skills-backend">
+                <div className="skills-backend group-divs">
                     <div>
                         <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/7/73/Ruby_logo.svg/1200px-Ruby_logo.svg.png" alt="" />
                         <p>Ruby</p>
@@ -42,23 +47,29 @@ class Skills extends React.Component {
                         <p>Ruby on Rails</p>
                     </div>
                 </div>
+                </div>
                 <p className="skill-titles">Mobile</p>
-                <div className="skills-mobile">
+                <div className="skills-mobile group-divs">
                     <div>
                         <img src="https://cdn.worldvectorlogo.com/logos/react-1.svg" alt=""/>
                         <p>React Native</p>
                     </div>
                 </div>
-                <p className="skill-titles">Other</p>
-                <div className="skills-other">
+                <div className="span-color">
+                <p className="skill-titles">Database</p>
+                <div className="skills-database group-divs">
                     <div>
                         <img src="https://azure.microsoft.com/svghandler/sql-database/?width=600&height=615" alt="" />
                         <p>SQL</p>
                     </div>
                     <div>
-                        <img src="https://js.foundation/wp-content/uploads/sites/33/2017/02/jquery.png" alt="" />
-                        <p>jQuery</p>
+                        <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/2/29/Postgresql_elephant.svg/1200px-Postgresql_elephant.svg.png" alt="" />
+                        <p>PostgreSQL</p>
                     </div>
+                </div>
+                </div>
+                <p className="skill-titles">Other</p>
+                <div className="skills-other group-divs">
                     <div>
                         <img src="https://png.icons8.com/color/540/heroku.png" alt="" />
                         <p>Heroku</p>
