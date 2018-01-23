e3109c533c999e9bb03579128657e03a72582e1d
Patrick Conde <patrick.conde.costas@gmail.com>
2018-01-22 23:51:44 -0800

    Almost finished about

--- frontend/components/about/about.jsx
+++ frontend/components/about/about.jsx
@@ -3,34 +3,43 @@ import React from 'react';
 class About extends React.Component {
     render() {
         return (
-            <div>
-                <div className="about-title">
-                    <p>
-                        Hello, I'm Patrick Conde!
-                    </p>
+            <div className="about-container">
+                <div>
+                    <p id="about-title">Hello, I'm Patrick!</p>
                 </div>
                 <div className="about-growth">
-                    <p>A little history of myself. I grew up in the US, Sweden, Spain, and Japan. English and Swedish I speak fluently,
-                    Spanish, I can hold a minor conversation, and my Japanese has faded somewhat, but I'm still studying it part-time.
+                    <div className="flags">
+                        <img src="https://upload.wikimedia.org/wikipedia/en/thumb/a/a4/Flag_of_the_United_States.svg/1280px-Flag_of_the_United_States.svg.png" alt=""/>
+                        <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/4/4c/Flag_of_Sweden.svg/1200px-Flag_of_Sweden.svg.png" alt=""/>
+                        <img src="https://upload.wikimedia.org/wikipedia/en/thumb/9/9a/Flag_of_Spain.svg/1200px-Flag_of_Spain.svg.png" alt=""/>
+                        <img src="https://upload.wikimedia.org/wikipedia/en/thumb/9/9e/Flag_of_Japan.svg/1280px-Flag_of_Japan.svg.png" alt=""/>
+                    </div>
+                    <p>I grew up in the US, Sweden, Spain, and Japan. English and Swedish I speak fluently,
+                    I can hold a minor conversation in Spanish, and although my Japanese has faded somewhat, 
+                    I'm still studying it part-time as a hobby.
                     </p>
                 </div>
-                <div className="about-growth">
+                <div className="about-rails">
                     <p>
-                    My programming education began primarily when I was living in Denmark, diving into the Rails scene.
+                    My programming education began primarily when I was living in Denmark, becoming enamored with the growing Rails framework.
                     Ruby was my first love, and Rails along with it. It became the foundation of my career, and I used my Ruby and Rails skills,
                     along with the HTML, CSS, JavaScript and jQuery knowledge I had accumulated, to make the move to San Francisco.
                     </p>
+                    <div className="flags-rails">
+                        <img src="https://cdn-images-1.medium.com/max/710/1*9hd_8qR0CMZ8L0pVbFLjDw.png" alt="" />
+                    </div>
                 </div>
                 <div className="about-sf">
+                    <div className="flags-react">
+                        <img src="https://cdn-images-1.medium.com/max/800/1*wjlfZatDlTnqfgYJ9iKnKQ.png" alt=""/>
+                    </div>
                     <p>
-                    I moved to San Francisco, where I've spent the last year harnessing my skills in React, Redux and React Native.
-                    I'm currently in the bay area, and open to getting in touch for more.
+                    In San Francisco, I've spent the last year harnessing my skills in React, Redux and React Native.
+                    I'm currently still in the bay area, looking for career opportunities.
                     </p>
                 </div>
                 <div className="about-connect">
-                    <p>
-                    Let's Connect: LinkedIn
-                    </p>
+                    <p>Let's Connect: <a className="linkedin-icon" target="_blank" href="https://www.linkedin.com/in/patrick-conde/"><i className="fa fa-linkedin-square" aria-hidden="true"></i></a></p>
                 </div>
             </div>
         )
