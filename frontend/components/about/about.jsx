f7b6b85dd7a70e3eefa3dcaba15b774d1671854b
Patrick Conde <patrick.conde.costas@gmail.com>
2018-01-22 21:07:53 -0800

    code organization

--- frontend/components/about/about.jsx
+++ frontend/components/about/about.jsx
@@ -4,24 +4,34 @@ class About extends React.Component {
     render() {
         return (
             <div>
-                <p>
-                    Hello, I'm Patrick Conde!
-                </p>
-                <p>A little history of myself. I grew up in the US, Sweden, Spain, and Japan. English and Swedish I speak fluently,
+                <div className="about-title">
+                    <p>
+                        Hello, I'm Patrick Conde!
+                    </p>
+                </div>
+                <div className="about-growth">
+                    <p>A little history of myself. I grew up in the US, Sweden, Spain, and Japan. English and Swedish I speak fluently,
                     Spanish, I can hold a minor conversation, and my Japanese has faded somewhat, but I'm still studying it part-time.
-                </p>
-                <p>
+                    </p>
+                </div>
+                <div className="about-growth">
+                    <p>
                     My programming education began primarily when I was living in Denmark, diving into the Rails scene.
                     Ruby was my first love, and Rails along with it. It became the foundation of my career, and I used my Ruby and Rails skills,
                     along with the HTML, CSS, JavaScript and jQuery knowledge I had accumulated, to make the move to San Francisco.
-                </p>
-                <p>
-                    I moved to San Francisco, where I've spent the last year harnessing my skills in React, Redux and React Native. 
-                    I'm currently in the bay area, and open to getting in touch for more. 
-                </p>
-                <p>
+                    </p>
+                </div>
+                <div className="about-sf">
+                    <p>
+                    I moved to San Francisco, where I've spent the last year harnessing my skills in React, Redux and React Native.
+                    I'm currently in the bay area, and open to getting in touch for more.
+                    </p>
+                </div>
+                <div className="about-connect">
+                    <p>
                     Let's Connect: LinkedIn
-                </p>
+                    </p>
+                </div>
             </div>
         )
     }
